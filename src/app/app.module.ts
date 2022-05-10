fdca8199975a60b4a626ce6361073809238eecea
Milindh Kashyap <milindhrk@gmail.com>
2022-05-10 13:02:20 +0530

    final commit 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UtilityComponent } from './utility/utility.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule } from '@angular/forms';
+import { TeamComponent } from './team/team.component';
+import {HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilityComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    EmployeeComponent,
+    TeamComponent,
   ],
   imports: [
+   
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
